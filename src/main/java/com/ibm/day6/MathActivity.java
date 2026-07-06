608244b08095c973c0b18e6957d6ad319cc161b6
sisonjcs <jcssison1@gmail.com>
2026-07-06 15:39:28 +0800

    docs: added javadoc

--- src/main/java/com/ibm/day6/MathActivity.java
+++ src/main/java/com/ibm/day6/MathActivity.java
@@ -1,18 +1,43 @@
 package com.ibm.day6;
 
 public class MathActivity {
+	/**
+	 * Adding two floating values
+	 * @param a
+	 * @param b
+	 * @return a+b
+	 */
 	public static float add(float a, float b) {
 		return a+b;
 	}
 	
+	/**
+	 * Subtracts b from a
+	 * @param a
+	 * @param b
+	 * @return a-b
+	 */
 	public static float subtract(float a, float b) {
 		return a-b;
 	}
 	
+	/**
+	 * Multiplies two floating values
+	 * @param a
+	 * @param b
+	 * @return a*b
+	 */
 	public static float multiply(float a, float b) {
 		return a*b;
 	}
 	
+	/**
+	 * Divides two floating values
+	 * @param a
+	 * @param b
+	 * @return a/b
+	 * @throws ArithmeticException
+	 */
 	public static float divide(float a, float b) throws ArithmeticException {
 		if (b == 0) {
 			throw new ArithmeticException("b cannot be 0");
